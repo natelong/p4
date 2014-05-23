3d6d30d207280f3eeac29db2471720612df43acc
Greg Cochard <greg.cochard@gmail.com>
2014-05-23 11:05:20 -0700

    Adding lots of stuff: - cwd -- keeps track of working dir and sets exec to use it. - cd -- lets you change cwd - stat -- stats the file and parses it into json - statDir -- runs stat on all files in the directory (*) - recursiveStatDir -- runs stat on all files from cwd (...) - revert -- reverts the file given in the args - submit -- submits the file given in the filepath arg, with description   given in desc arg - sync -- syncs the file in the args to the head rev - syncDir -- runs sync on * - recursiveSyncDir -- runs sync on ... - have -- stats the file and grabs haveRev - login -- logs in with the username and password given

--- p4.js
+++ p4.js
@@ -1,40 +1,154 @@
 /*jshint node:true*/
+/*jslint node:true*/
 "use strict";
 
 var exec = require("child_process").exec;
+var path = require("path");
+var cwd = __dirname;
 
 function runCommand(command, args, done) {
     if(typeof args === "function") {
         done = args;
         args = "";
     }
+    if(typeof args === 'array') {
+        args = args.join(' ');
+    }
 
-    exec("p4 " + command + " " + (args || ""), function(err, stdOut, stdErr) {
-        if(err) return done(err);
-        if(stdErr) return done(new Error(stdErr));
+    exec("p4 " + command + " " + (args || ""), {cwd:cwd}, function(err, stdOut, stdErr) {
+        if(err) {return done(err);}
+        if(stdErr) {return done(new Error(stdErr));}
 
         done(null, stdOut);
     });
 }
 
-function edit(path, done) {
-    runCommand("edit", path, done);
-}
+function runShellCommand(command, args, done) {
+    if(typeof args === "function") {
+        done = args;
+        args = "";
+    }
+    if(typeof args === 'array') {
+        args = args.join(' ');
+    }
 
-function add(path, done) {
-    runCommand("add", path, done);
-}
+    exec(command + " " + (args || ""), {cwd:cwd}, function(err, stdOut, stdErr) {
+        if(err) {return done(err);}
+        if(stdErr) {return done(new Error(stdErr));}
 
-function smartEdit(path, done) {
-    edit(path, function(err) {
-        if(!err) return done();
-
-        add(path, done);
+        done(null, stdOut);
     });
 }
 
-function revertUnchanged(path, done) {
+function edit(filepath, done) {
+    runCommand("edit", filepath, done);
+}
+
+function add(filepath, done) {
+    runCommand("add", filepath, done);
+}
+
+function smartEdit(filepath, done) {
+    edit(filepath, function(err) {
+        if(!err) {return done();}
+
+        add(filepath, done);
+    });
+}
+
+function revertUnchanged(filepath, done) {
     runCommand("revert", "-a", done);
+}
+
+function parseStats(stats) {
+    var statsObj = {}
+      , lastKey;
+    stats = stats.split('\n');
+    stats.forEach(function(line){
+        var level = 0
+          , key = ''
+          , value = ''
+          , obj = [];
+        //line has 3 dots and a space at the beginning, pull that off
+        do{
+            line = line.slice(4);
+            level++;
+        }
+        while(0===line.indexOf('... '));
+        obj = line.split(' ');
+        obj[1] = obj.slice(1).join(' ');
+        key = obj[0];
+        value = obj[1];
+        if(!key){ return; }
+        if(value === ''){
+            value = true;
+        }
+        if(level === 1){
+            statsObj[key] = value;
+            //lastKey = key;
+        } else if(level === 2){
+            lastKey = 'other';
+            if(typeof statsObj[lastKey] !== 'object'){
+                statsObj[lastKey] = {};
+            }
+            statsObj[lastKey][key] = value;
+        }
+    });
+    return statsObj;
+}
+
+function statDir(filepath, done) {
+    runCommand('fstat', path.join(filepath,'*'), done);
+}
+
+function recursiveStatDir(filepath, done) {
+    runCommand('fstat', path.join(filepath,'...'), done);
+}
+
+function stat(filepath, done) {
+    runCommand('fstat', filepath, function(err,out){
+        if(err){return done(err);}
+        return done(null,parseStats(out));
+    });
+}
+
+function have(filepath, done) {
+    stat(filepath, function(err,stats){
+        if(err){return done(err);}
+        return done(null,stats.haveRev);
+    });
+}
+
+function revert(filepath, done) {
+    runCommand('revert', filepath, done);
+}
+
+function submit(filepath, desc, done) {
+    runCommand('submit', [filepath,'-d','"'+desc+'"'], done);
+}
+
+function sync(filepath, done) {
+    runCommand('sync', filepath, done);
+}
+
+function syncDir(filepath, done) {
+    runCommand('sync', path.join(filepath,'*'), done);
+}
+
+function recursiveSyncDir(filepath, done) {
+    runCommand('sync', path.join(filepath,'...'), done);
+}
+
+function login(username, password, done) {
+    runShellCommand('echo "'+password+'" | p4 login -u "'+username+'"', done);
+}
+
+function cd(dir){
+    cwd = path.join(cwd,dir);
+    //allow chaining by returning exports
+    //i.e. p4.cd('dir').edit('file')
+    //or p4.cd('path').cd('to').cd('dir')
+    return exports;
 }
 
 exports.edit = edit;
@@ -42,3 +156,14 @@ exports.add = add;
 exports.smartEdit = smartEdit;
 exports.run = runCommand;
 exports.revertUnchanged = revertUnchanged;
+exports.stat = stat;
+exports.statDir = statDir;
+exports.recursiveStatDir = recursiveStatDir;
+exports.revert = revert;
+exports.submit = submit;
+exports.sync = sync;
+exports.syncDir = syncDir;
+exports.recursiveSyncDir = recursiveSyncDir;
+exports.have = have;
+exports.login = login;
+exports.cd = cd;
