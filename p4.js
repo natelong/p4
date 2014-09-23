a6ca37fe5f096a75b3e9e5e9d5cd14d7d42a7ec2
Greg Cochard <greg@fitbeast.io>
2014-09-23 15:09:41 -0700

    Changing from exports.* style to object-oriented. Should support having multiple instances of p4, which is conducive to a web server maintaining state across all users. Lets us use perforce without having to pass options every time we run any command.

--- p4.js
+++ p4.js
@@ -1,32 +1,37 @@
-/*jshint node:true*/
 /*jslint node:true*/
 "use strict";
 
 var exec = require("child_process").exec;
 var path = require("path");
-var cwd = __dirname;
-var options = {};
 
-function cd(dir){
-    cwd = path.resolve(cwd,dir);
-    //allow chaining by returning exports
-    //i.e. p4.cd('dir').edit('file')
-    //or p4.cd('path').cd('to').cd('dir')
-    return exports;
+function P4(){
+    if(!this instanceof P4){
+        return new P4();
+    }
+    this.cwd = __dirname;
+    this.options = {};
 }
 
-function setOpts(opts){
+P4.prototype.cd = function(dir){
+    this.cwd = path.resolve(this.cwd,dir);
+    //allow chaining by returning this
+    //i.e. p4.cd('dir').edit('file')
+    //or p4.cd('path').cd('to').cd('dir')
+    return this;
+};
+
+P4.prototype.setOpts = function(opts){
     Object.keys(opts).forEach(function(key){
         if(key === 'cwd'){
             //don't let them change cwd via setOpts...
             return;
         }
-        options[key] = opts[key];
+        this.options[key] = opts[key];
     });
-    return exports;
-}
+    return this;
+};
 
-function runCommand(command, args, done) {
+P4.prototype.runCommand = function(command, args, done) {
     if(typeof args === "function") {
         done = args;
         args = "";
@@ -35,9 +40,9 @@ function runCommand(command, args, done) {
         args = args.join(' ');
     }
 
-    options.cwd = cwd;
-    options.env.PWD = cwd;
-    exec("p4 " + command + " " + (args || ""), options, function(err, stdOut, stdErr) {
+    this.options.cwd = this.cwd;
+    this.options.env.PWD = this.cwd;
+    exec("p4 " + command + " " + (args || ""), this.options, function(err, stdOut, stdErr) {
         if(err) {return done(err);}
         //when we run p4 fstat *, it will say no such file(s) on dirs
         //fix this by reducing the error string and omitting matching lines
@@ -49,14 +54,17 @@ function runCommand(command, args, done) {
             }
             return pval;
         },[]).join('\n');
-        //this could more easily be done with stdErr = _.reject(stdErr.split('\n'),function(line){return /no such file/.test(line);}).join('\n') but learning reduce is a _good thing_ (TM) and the algorithm is arguably more descriptive and self-documenting when using reduce
+        //this could more easily be done with this:
+        //stdErr = _.reject(stdErr.split('\n'),function(line){return /no such file/.test(line);}).join('\n')
+        //but learning reduce is a _good thing_ (TM) and the algorithm is arguably more descriptive and 
+        //self-documenting when using reduce
         if(stdErr) {return done(new Error(stdErr));}
 
         done(null, stdOut);
     });
-}
+};
 
-function runShellCommand(command, args, done) {
+P4.prototype.runShellCommand = function(command, args, done) {
     if(typeof args === "function") {
         done = args;
         args = "";
@@ -65,35 +73,37 @@ function runShellCommand(command, args, done) {
         args = args.join(' ');
     }
 
-    exec(command + " " + (args || ""), {cwd:cwd}, function(err, stdOut, stdErr) {
+    exec(command + " " + (args || ""), {cwd:this.cwd}, function(err, stdOut, stdErr) {
         if(err) {return done(err);}
         if(stdErr) {return done(new Error(stdErr),stdOut);}
 
         done(null, stdOut);
     });
-}
+};
 
-function edit(filepath, done) {
-    runCommand("edit", filepath, done);
-}
+P4.prototype.edit = function(filepath, done) {
+    this.runCommand("edit", filepath, done);
+};
 
-function add(filepath, done) {
-    runCommand("add", filepath, done);
-}
+P4.prototype.add = function(filepath, done) {
+    this.runCommand("add", filepath, done);
+};
 
-function smartEdit(filepath, done) {
-    edit(filepath, function(err) {
+P4.prototype.smartEdit = function(filepath, done) {
+    var self = this;
+    this.edit(filepath, function(err) {
         if(!err) {return done();}
 
-        add(filepath, done);
+        self.add(filepath, done);
     });
-}
+};
+/*jslint unparam:true*/
+P4.prototype.revertUnchanged = function(filepath, done) {
+    this.runCommand("revert", "-a", done);
+};
+/*jslint unparam:false*/
 
-function revertUnchanged(filepath, done) {
-    runCommand("revert", "-a", done);
-}
-
-function parseStats(stats) {
+P4.prototype.parseStats = function(stats) {
     var statsArr = [];
     stats = stats.split('\n\n');
     stats.forEach(function(stat){
@@ -138,92 +148,77 @@ function parseStats(stats) {
         return statsArr[0];
     }
     return statsArr;
-}
+};
 
-function statDir(filepath, done) {
+P4.prototype.statDir = function(filepath, done) {
+    var self = this;
     if(filepath){
-        cd(filepath);
+        this.cd(filepath);
     }
-    runCommand('fstat','*', function(err,out){
+    this.runCommand('fstat','*', function(err,out){
         if(err){return done(err);}
-        return done(null,parseStats(out));
+        return done(null,self.parseStats(out));
     });
-}
+};
 
-function recursiveStatDir(filepath, done) {
+P4.prototype.recursiveStatDir = function(filepath, done) {
+    var self = this;
     if(filepath){
-        cd(filepath);
+        this.cd(filepath);
     }
-    runCommand('fstat','...', function(err,out){
+    this.runCommand('fstat','...', function(err,out){
         if(err){return done(err);}
-        return done(null,parseStats(out));
+        return done(null,self.parseStats(out));
     });
-}
+};
 
-function stat(filepath, done) {
-    cd(path.dirname(filepath));
-    runCommand('fstat', path.basename(filepath), function(err,out){
+P4.prototype.stat = function(filepath, done) {
+    var self = this;
+    this.cd(path.dirname(filepath));
+    this.runCommand('fstat', path.basename(filepath), function(err,out){
         if(err){return done(err);}
-        return done(null,parseStats(out));
+        return done(null,self.parseStats(out));
     });
-}
+};
 
-function have(filepath, done) {
-    stat(filepath, function(err,stats){
+P4.prototype.have = function(filepath, done) {
+    this.stat(filepath, function(err,stats){
         if(err){return done(err);}
         return done(null,stats.haveRev);
     });
-}
+};
 
-function revert(filepath, done) {
-    runCommand('revert', filepath, done);
-}
+P4.prototype.revert = function(filepath, done) {
+    this.runCommand('revert', filepath, done);
+};
 
-function submit(filepath, desc, done) {
-    runCommand('submit', [filepath,'-d','"'+desc+'"'], done);
-}
+P4.prototype.submit = function(filepath, desc, done) {
+    this.runCommand('submit', [filepath,'-d','"'+desc+'"'], done);
+};
 
-function sync(filepath, done) {
-    runCommand('sync', filepath, done);
-}
+P4.prototype.sync = function(filepath, done) {
+    this.runCommand('sync', filepath, done);
+};
 
-function syncDir(filepath, done) {
+P4.prototype.syncDir = function(filepath, done) {
     if(filepath){
-        cd(filepath);
+        this.cd(filepath);
     }
-    runCommand('sync', '*', done);
-}
+    this.runCommand('sync', '*', done);
+};
 
-function recursiveSyncDir(filepath, done) {
+P4.prototype.recursiveSyncDir = function(filepath, done) {
     if(filepath){
-        cd(filepath);
+        this.cd(filepath);
     }
-    runCommand('sync', '...', done);
-}
+    this.runCommand('sync', '...', done);
+};
 
-function login(username, password, done) {
-    runShellCommand('echo "'+password+'" | p4 login -u "'+username+'"', done);
-}
+P4.prototype.login = function(username, password, done) {
+    this.runShellCommand('echo "'+password+'" | p4 login -u "'+username+'"', done);
+};
 
-function pwd(){
-    return cwd;
-}
-
-exports.edit = edit;
-exports.add = add;
-exports.smartEdit = smartEdit;
-exports.run = runCommand;
-exports.revertUnchanged = revertUnchanged;
-exports.stat = stat;
-exports.statDir = statDir;
-exports.recursiveStatDir = recursiveStatDir;
-exports.revert = revert;
-exports.submit = submit;
-exports.sync = sync;
-exports.syncDir = syncDir;
-exports.recursiveSyncDir = recursiveSyncDir;
-exports.have = have;
-exports.login = login;
-exports.cd = cd;
-exports.pwd = pwd;
-exports.setOpts = setOpts;
+P4.prototype.pwd = function(){
+    return this.cwd;
+};
+module.exports = P4;
